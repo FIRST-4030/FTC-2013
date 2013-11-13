ce36860aa35c5aa2f53c6bc462f594928c5a0d8f
Zachary Kotlarek <zach@kotlarek.com>
2013-11-13 02:26:21 -0800

    Better file splits

--- Drive/lines.c
+++ Drive/lines.c
@@ -1,46 +1,76 @@
 // Store the light sensor ports for use in line functions
-tSensors lightLeft  = null;
-tSensors lightRight = null;
-void setLightSensors(tSensors left, tSensors right) {
-	lightLeft  = left;
-	lightRight = right;
+tSensors lineLeft  = null;
+tSensors lineRight = null;
+void setLineSensors(tSensors left, tSensors right) {
+	lineLeft  = left;
+	lineRight = right;
 }
 
-// Follow a line, after we've been aligned to it
-void FollowLine(FloorColor color, int speed) {
-	if(onColor(color, LSvalRaw(lightLeft))) {
-		runMotors(0, speed);
-	} else if(onColor(color, LSvalRaw(lightRight))) {
-		driveMotors(speed, 0);
-	} else {
-		driveMotors(speed, speed);
+// Follow a line, after we've been aligned to it, stopping when we hit the STOP color
+void followLineToColor(FloorColor color, int speed, FloorColor stop) {
+	_folowLine(color, speed, stop, 0, 0);
+}
+
+// Follow a line, after we've been aligned to it, stopping based on color, distance, or time
+void _followLine(FloorColor color, int speed, FloorColor stopColor, int stopDistance, int stopTime) {
+	// Reset the drive encoder if we're going to use it
+	if (stopDistance != 0) {
+		resetDriveEncoder();
 	}
+	
+	// Loop until we hit a stop condition
+	while (true) {
+		if (stopColor != UNKNOWN && (onColor(stop, LSvalRaw(lineLeft)) || onColor(stop, LSvalRaw(lineRight)))) {
+			break;
+		} else if (stopDistance > 0 && readDriveEncoder() > stopDistance) {
+			break;
+		} else if (stopDistance < 0 && readDriveEncoder() < stopDistance) {
+			break;
+		} else if (stopTime > 0 && true) {
+			// Can we check elapsed time?
+			break;
+		}
+
+		if (onColor(color, LSvalRaw(lineLeft))) {
+			runDriveMotors(0, speed);
+		} else if (onColor(color, LSvalRaw(lineRight))) {
+			runDriveMotors(speed, 0);
+		} else {
+			runDriveMotors(speed, speed);
+		}
+	}
+	
+	// Always stop when we're done
+	stopDriveMotors();
 }
 
 // Align to a line, assuming we start at or just beyond it
 void alignLine(FloorColor color, int speed) {
-	alignLineStop(color, speed, UNKNOWN);
+	_alignLine(color, speed, UNKNOWN);
 }
 
 // Align to a line, assuming we start at or just beyond it, but stop if we hit the STOP color with either sensor
-void alignLineStop(FloorColor color, int speed, FloorColor stop) {
+void _alignLine(FloorColor color, int speed, FloorColor stop) {
 	
 	// When the right sensor is on the line, we're aligned
-	while(!onColor(color, LSvalRaw(lightRight))) {
+	while(!onColor(color, LSvalRaw(lineRight))) {
 		
 		// Stop alignment efforts if we hit the specified stop floor color
-		if (stop != UNKONWN && (onColor(stop, LSvalRaw(lightLeft)) || onColor(stop, LSvalRaw(lightRight)))) {
+		if (stop != UNKNOWN && (onColor(stop, LSvalRaw(lineLeft)) || onColor(stop, LSvalRaw(lineRight)))) {
 			break;
 		}
 
 		// Move forward while the left sensor is on the line
-		while(onColor(color, LSvalRaw(lightLeft))) {
-			runMotors(speed, speed);
+		while(onColor(color, LSvalRaw(lineLeft))) {
+			runDriveMotors(speed, speed);
 		}
 
 		// Turn clockwise (right) when neither sensor is on the line
-		while(!onColor(color, LSvalRaw(lightLeft)) && !onColor(color, LSvalRaw(lightRight))) {
-			runMotors(speed, 0);
+		while(!onColor(color, LSvalRaw(lineLeft)) && !onColor(color, LSvalRaw(lineRight))) {
+			runDriveMotors(speed, 0);
 		}
 	}
+	
+	// Always stop when we're done
+	stopDriveMotors();
 }
--- Motors/motors.c
+++ Motors/motors.c
@@ -1,15 +1,20 @@
 // Store the motor ports for use in drive functions
 tMotor driveLeft  = null;
 tMotor driveRight = null;
-void setMotors(tMotor left, tMotor right) {
+void setDriveMotors(tMotor left, tMotor right) {
 	driveLeft  = left;
 	driveRight = right;
 }
 
 // Run motors at the specified speed indefinately
-void runMotors(int left, int right) {
+void runDriveMotors(int left, int right) {
 	motor[driveLeft]  = left;
 	motor[driveRight] = right;	
+}
+
+// Stop all drive motion
+void stopDriveMotors() {
+	runMotors(0, 0);
 }
 
 // Run motors at the specified speed, stopping after DURATION milliseconds
@@ -18,4 +23,13 @@ void driveMotors(int left, int right, const int duration) {
 	if (duration >= 0) {
 		wait1Msec(duration);
 	}
+}
+
+// Reset the left drive encoder
+void resetDriveEncoder() {
+	nMotorEncoder[driveLeft] = 0;
+}
+
+int readDriveEncoder() {
+	return nMotorEncoder[driveLeft];
 }
