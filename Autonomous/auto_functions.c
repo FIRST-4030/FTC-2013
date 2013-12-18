b968ec7d83ded2b6765c54b0403974b46621cf58
Team Laptop <frc@uberzach.com>
2013-12-17 18:16:58 -0800

    Encapsulate the basket-ramp routine

--- Autonomous/auto_functions.c
+++ Autonomous/auto_functions.c
@@ -144,4 +144,110 @@ void Wall_DirectToRamp() {
 		runDriveMotors(HALF_IMPULSE,HALF_IMPULSE);
 	}
 }
+
+// Tasks for background lift movement
+bool IS_LIFT_UP = false;
+task liftUp() {
+	IS_LIFT_UP = false;
+	MoveLift(false);
+	IS_LIFT_UP = true;
+}
+task liftDown() {
+	IS_LIFT_UP = true;
+	MoveLift(true);
+	IS_LIFT_UP = false;
+}
+
+// Drive to the IR beacon, dump in the basket, return to start, drive up ramp
+void autoBasketRamp(START_SIDE side = RIGHT) {
+	// Start raising the lift so it's ready when we arrive
+	StartTask(liftUp);
+
+	// Drive to the IR beacon, recording our distance
+	resetDriveEncoder();
+	bool validIR = driveToIR();
+	int traveled = readDriveEncoder();
+
+	// Figure out which basket we're at
+	// If we didn't get a valid IR run, prefer the 1st basket
+	int basket = 0;
+	if (validIR) {
+		// If nothing matches we're at the first basket, so default to 0 if IR is valid
+		basket = 0;
+		for (int i = NUM_BASKETS - 1; i > 0; i--) {
+			// Since the sensor is behind the midpoint we should always overshoot
+			if (traveled >= basketPositions[side][i]) {
+				basket = i;
+				break;
+			}
+		}
+	}
+
+	// Adjust back to the correct turn point for the selected basket
+	int adjustSpeed = HALF_IMPULSE;
+	int adjustDistance = 0;
+	// In theory we can just drive to the basket position but our encoder
+	// is pretty sloppy, so put more trust in the IR when available
+	if (validIR) {
+
+		// Back up a bit to get aligned -- alignment varies between baskets 1/2 and 3/4
+		if (basket > 1) {
+			adjustDistance = -1000;
+		} else {
+			adjustDistance = -400;
+		}
+
+		// We don't turn symetrically, so adjust when we're starting on the left
+		if (START_SIDE == LEFT) {
+			adjustDistance += 400;
+		}
+	} else {
+		adjustDistance = (basketPositions[side][basket] - traveled);
+	}
+	if (adjustDistance < 0) {
+		adjustSpeed *= -1;
+	}
+	driveToDistance(adjustDistance, adjustSpeed);
+
+	// Turn to face baskets
+	turnInPlaceDegrees(90, HALF_IMPULSE, (bool)side);
+
+	// Wait for the lift to be up
+	while (!IS_LIFT_UP) {
+		wait1Msec(100);
+	}
+
+	// Move forward to basket
+	ApproachBasket();
+
+	// Dump
+	DumpHopper();
+
+	// Nudge back for safety
+	driveToDistance(-400, -FULL_IMPULSE);
+
+	// Star lowering the lift
+	StartTask(liftDown);
+
+	// Turn back to original orientation
+	turnInPlaceDegrees(90, FULL_IMPULSE, (!(bool)side));
+
+	// Drive backward to the corner
+	driveToDistance((-1 * (traveled + adjustDistance)), -FULL_IMPULSE);
+
+	// Drive back some more to get aligned with the wall
+	driveToDistance(-3000, -FULL_IMPULSE);
+
+	// Turn to avoid ramp
+	turnInPlaceDegrees(70, FULL_IMPULSE, (bool)side);
+
+	// Drive to white line
+	driveToColor(WHITE, FULL_IMPULSE);
+
+	// Turn to ramp
+	turnInPlaceDegrees(95, FULL_IMPULSE);
+
+	// Drive up ramp
+	driveToDistance(8000, FULL_IMPULSE);
+}
 #endif
