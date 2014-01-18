b098b38b8e7b877832845d1e0b6fec9edc0fc57b
Team Laptop <frc@uberzach.com>
2014-01-17 19:08:52 -0800

    Improve basket estimates for turn-left operation

--- Autonomous/auto_init.c
+++ Autonomous/auto_init.c
@@ -11,15 +11,15 @@ typedef enum {
 int basketPositions[NUM_START_SIDES][NUM_BASKETS];
 
 void initBasketPositions() {
-	basketPositions[LEFT][0] = 1250;
-	basketPositions[LEFT][1] = 2200;
-	basketPositions[LEFT][2] = 4850;
-	basketPositions[LEFT][3] = 6600;
-
 	basketPositions[RIGHT][0] = 1250;
 	basketPositions[RIGHT][1] = 2200;
 	basketPositions[RIGHT][2] = 4850;
 	basketPositions[RIGHT][3] = 6600;
+
+	basketPositions[LEFT][0] = basketPositions[RIGHT][0] + 700;
+	basketPositions[LEFT][1] = basketPositions[RIGHT][1] + 700;
+	basketPositions[LEFT][2] = basketPositions[RIGHT][2] + 700;
+	basketPositions[LEFT][3] = basketPositions[RIGHT][3] + 700;
 }
 
 void AutonomousInit() {
