72a33ccb4d7bf2b9d95ce1d357348b7f56eac0ee
shandamelcher <git@shananda.com>
2014-01-19 03:47:53 -0800

    Get the gyro working

--- Sensors/gyro.c
+++ Sensors/gyro.c
@@ -1,30 +1,46 @@
 #ifndef FTC_GYRO
 #define FTC_GYRO
 
-#define GYRO_PERIOD (20)
 #include "../drivers/hitechnic-gyro.h"
 
+#define GYRO_PERIOD (10)
+#define GYRO_FLOAT_SPEED (10.0)
+#define GYRO_CAL_SAMPLES (50)
+
+bool GYRO_READY = false;
 float GYRO_ANGLE = 0.0;
 
 task gyro() {
+	GYRO_READY = false;
 	GYRO_ANGLE = 0.0;
-	HTGYROstartCal(gyroSensor);
-	wait1Msec(100);
 
-	time1[T1] = 0;
+	// Calibrate -- assume we are stationary
+	float cal = 0.0;
+	HTGYROsetCal(gyroSensor, 0);
+	for (int i = 0; i < GYRO_CAL_SAMPLES; i++) {
+		cal += HTGYROreadRot(gyroSensor);
+		wait1Msec(5);
+	}
+	HTGYROsetCal(gyroSensor, cal / (float)GYRO_CAL_SAMPLES);
+
+	time1[T4] = 0;
 	while (true) {
 
-		// Wait 20ms in small slices
+		// Wait in small slices
 		while (time1[T4] < GYRO_PERIOD) {
 			wait1Msec(1);
 		}
 
 		// Immediately reset the timer in case we get de-scheduled
-		time1[T1] = 0;
+		time1[T4] = 0;
 
 		// Read and integrate
 		float speed = HTGYROreadRot(gyroSensor);
-		GYRO_ANGLE += speed * (GYRO_PERIOD / 1000.0);
+		if (abs(speed) < GYRO_FLOAT_SPEED) {
+			speed = 0.0;
+		}
+		GYRO_ANGLE += speed * ((float)GYRO_PERIOD / 1000.0);
+		GYRO_READY = true;
 	}
 }
 
